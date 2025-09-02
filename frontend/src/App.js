a2cc36595b139d355eff084a19af03397ddc0372
jsmcrsvp <jsvp.rsvp@gmail.com>
2025-09-02 09:05:25 -0500

    Sept022025

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+//import LoginForm from './components/LoginForm';
+//import TablePage from './components/TablePage';
+//import keepServerAlive from './components/KeepServerAlive';
+//import ReportPage from './components/ReportPage';
+import './styles/App.css';
+
+function App() {
+  const [pollRefreshTrigger, setPollRefreshTrigger] = useState(0);
+
+  useEffect(() => {
+    keepServerAlive();
+  }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/data" element={<TablePage pollRefreshTrigger={pollRefreshTrigger} setPollRefreshTrigger={setPollRefreshTrigger} />} />
+        <Route path="/report" element={<ReportPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
