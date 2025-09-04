7fb75dc6f8f69526cad1cfc36b7e2eb9307d17b1
jsmcrsvp <jsvp.rsvp@gmail.com>
2025-09-03 21:27:46 -0500

    Sept032025

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import SubmitForm from './components/SubmitForm';
-import AddProgramForm from "./components/AddProgramForm";
-//import TablePage from './components/TablePage';
-//import keepServerAlive from './components/KeepServerAlive';
-//import ReportPage from './components/ReportPage';
+//import SubmitForm from './components/SubmitForm';  // ❌ old member search
+import SubmitRSVPForm from './components/SubmitRSVPForm'; // ✅ new RSVP form
+import AddProgramForm from './components/AddProgramForm';
 import './styles/App.css';
 
 function App() {
-  const [pollRefreshTrigger, setPollRefreshTrigger] = useState(0);
-
-/*  useEffect(() => {
-    keepServerAlive();
-  }, []);*/
-
-    return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SubmitForm />} />
-        <Route path="/add-program" element={<AddProgramForm />} />
-      </Routes>
-    </Router>
-  );
-  /*
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SubmitForm />} />
-        <Route path="/data" element={<TablePage pollRefreshTrigger={pollRefreshTrigger} setPollRefreshTrigger={setPollRefreshTrigger} />} />
-        <Route path="/report" element={<ReportPage />} />
+        {/* Replace old default page with RSVP form */}
+        <Route path="/" element={<SubmitRSVPForm />} />
+
+        {/* Add program still works */}
+        <Route path="/add-program" element={<AddProgramForm />} />
+
+        {/* Optional: direct route to RSVP form */}
+        <Route path="/submit-rsvp" element={<SubmitRSVPForm />} />
       </Routes>
     </Router>
-  );*/
+  );
 }
 
 export default App;
