0d110a24fedfcce0f3a05133ef8974e7683d7845
jsmcrsvp <jsvp.rsvp@gmail.com>
2025-09-03 22:30:11 -0500

    Sept032025

--- backend/routes/rsvp.js
+++ backend/routes/rsvp.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const RSVP = require("../models/RSVPResponse"); // new model for rsvp_response
+const RSVP = require("../models/Rsvp_Response_DB_Schema"); // new model for rsvp_response
 
 router.post("/", async (req, res) => {
   try {
