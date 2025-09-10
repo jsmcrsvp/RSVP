00931e2670a55e9501b928a8f76f616a6745c0e9
jsmcrsvp <jsvp.rsvp@gmail.com>
2025-09-10 10:22:04 -0500

    Sept102025

--- backend/models/Programs_DB_Schema.js
+++ backend/models/Programs_DB_Schema.js
@@ -5,7 +5,8 @@ const eventSchema = new mongoose.Schema({
   eventname: { type: String, required: true },
   eventdate: { type: String, required: true },
   eventday: { type: String, required: true },
-  eventstatus: { type: String, required: true, enum: ["Open", "Closed", "Completed"] }
+  eventstatus: { type: String, required: true, enum: ["Open", "Closed", "Completed"] },
+  closersvp: { type: String, required: true }
 });
 
 // Define Program schema
