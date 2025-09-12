edce55ddae32e75254400b97697dbebcda80af9f
jsmcrsvp <jsvp.rsvp@gmail.com>
2025-09-12 16:52:17 -0500

    Sept122025

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,26 @@
+import React, { useState } from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import SubmitRSVP from './components/SubmitRSVP';
+import AddProgramForm from './components/AddProgramForm';
+import Admin from './components/Admin';
+import './styles/App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SubmitRSVP />} />
+        <Route path="/add-program" element={<AddProgramForm />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/submit-rsvp" element={<SubmitRSVP />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+/*
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import RSVPPage from "./components/RSVPPage";
@@ -10,11 +33,11 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Default route → Home */}
-        {/*<Route path="/" element={<Home />} />*/}
+        {/* Default route → Home
+        {/*<Route path="/" element={<Home />} />
         <Route path="/" element={<RSVPPage />} />
 
-        {/* Explicit routes */}
+        {/* Explicit routes
         <Route path="/home" element={<Home />} />
         <Route path="/member-rsvp" element={<MemberRSVP />} />
         <Route path="/nonmember-rsvp" element={<NonMemberRSVP />} />
@@ -22,9 +45,9 @@ function App() {
         <Route path="/submit-rsvp" element={<SubmitRSVPLanding />} />
         
         {/* Admin Page
-        <Route path="/admin" element={<Admin />} /> */}
+        <Route path="/admin" element={<Admin />} />
 
-        {/* Catch-all redirect */}
+        {/* Catch-all redirect
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
@@ -32,35 +55,6 @@ function App() {
 }
 
 export default App;
-
-
-/*
-  import React, { useState } from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import SubmitRSVP from './components/SubmitRSVP';
-import AddProgramForm from './components/AddProgramForm';
-import Admin from './components/Admin';
-import './styles/App.css';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Replace old default page with RSVP form
-        <Route path="/" element={<SubmitRSVP />} />
-
-        {/* Add program still works
-        <Route path="/add-program" element={<AddProgramForm />} />
-
-        {/* Admin Page
-        <Route path="/admin" element={<Admin />} />
-
-        {/* Optional: direct route to RSVP form
-        <Route path="/submit-rsvp" element={<SubmitRSVP />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
 */
+
+
