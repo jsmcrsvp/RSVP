6a2107efc8f250a2ac4d751fc0d006611c32999f
jsmcrsvp <jsvp.rsvp@gmail.com>
2025-09-12 12:39:23 -0500

    Sept122025

--- frontend/src/components/SubmitRSVP/MemberRSVP.js
+++ frontend/src/components/SubmitRSVP/MemberRSVP.js
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { submitRSVP } from "../../api";
 //import "...../styles/SubmitRSVP.css"
-import ".../styles/SubmitRSVP.css"
+import ".../styles/SubmitRSVP.css";
 
 export default function MemberRSVP({ events, onHome }) {
 
