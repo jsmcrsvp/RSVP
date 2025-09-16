b6d6e07e9dd2dad5d3f6e3d6e14f49a66d0ee669
jsmcrsvp <jsvp.rsvp@gmail.com>
2025-09-15 23:40:34 -0500

    Sept152025

--- frontend/src/components/AdminAddEventForm.js
+++ frontend/src/components/AdminAddEventForm.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getAllEvents, addNewEvent } from "../api";
+import "../styles/Admin.css";
 
 const AdminAddEvent = () => {
     const [eventName, setEventName] = useState("");
--- frontend/src/components/AdminAddProgramForm.js
+++ frontend/src/components/AdminAddProgramForm.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getAdminAllPrograms, addAdminNewProgram } from "../api";
+import "../styles/Admin.css";
 
 const AdminAddProgram = () => {
     const [programName, setProgramName] = useState("");
