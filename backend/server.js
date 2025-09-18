a52d58e8c287ccfe85ae0b42aac1062b547615d2
jsmcrsvp <jsvp.rsvp@gmail.com>
2025-09-18 11:45:15 -0500

    Sept182025

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.json());
 
 //=================== CORS Configuration ===================
 const corsOptions = {
