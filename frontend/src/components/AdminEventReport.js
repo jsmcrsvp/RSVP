97c699ffc06b2cfd35b51ff910fb36d471c2185a
jsmcrsvp <jsvp.rsvp@gmail.com>
2025-09-18 10:51:41 -0500

    Sept182025

--- frontend/src/components/AdminEventReport.js
+++ frontend/src/components/AdminEventReport.js
@@ -84,7 +84,7 @@ export default function AdminEventReport() {
       // Fetch member-level RSVP details
       const detailRes = await getRsvpDetails(selectedProgram, selectedEvent);
       //setMemberDetails(Array.isArray(detailRes.data) ? detailRes.data : []);
-      const setMemberDetails = (Array.isArray(detailRes.data) ? detailRes : []);
+      const setMemberDetails = (Array.isArray(detailRes) ? detailRes : []);
       console.log("Member stats response:", setMemberDetails);
 
       setError("");
