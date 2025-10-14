7d2146063dc16da1a50cec1273e8297af37544e1
jsmcrsvp <jsvp.rsvp@gmail.com>
2025-10-14 10:06:48 -0500

    Oct142025

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -22,7 +22,7 @@ export default function Admin() {
           <img src={logo} alt="JSMC Logo" className="rsvp-logo" />
         </div>
         <h2>JSMC RSVP Admin Portal</h2>
-        <h4>Welcome to JSMC RSVP Admin Portal</h4>
+        <h4>Select a tab to manage RSVP programs and events</h4>
 
         {/* Tabs */}
         <div className="tab-header">
@@ -46,7 +46,7 @@ export default function Admin() {
         {/* HOME */}
         {activeTab === "home" && (
           <div className="home">
-            <p>Select a tab to manage RSVP programs and events.</p>
+            {/*<p>Select a tab to manage RSVP programs and events.</p>*/}
           </div>
         )}
 
