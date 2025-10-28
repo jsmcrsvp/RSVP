7448f89abe343c96f607fac862cfab147e4f9704
jsmcrsvp <jsvp.rsvp@gmail.com>
2025-10-28 15:28:20 -0500

    Oct282025

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import SubmitRSVP from './components/SubmitRSVP';
+//import SubmitRSVP from './components/SubmitRSVP';
 import Home from './components/Home';
 import Admin from './components/Admin';
 import './styles/App.css';
