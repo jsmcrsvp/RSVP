d231a78a4ec1a05888119b05617cebe51604a344
jsmcrsvp <jsvp.rsvp@gmail.com>
2025-10-29 15:00:17 -0500

    Oct292025

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 //import SubmitRSVP from './components/SubmitRSVP';
 import Home from './components/Home';
 import Admin from './components/Admin';
-import TestHome from './components/TestHome';
 import './styles/App.css';
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
-        <Route path="/test" element={<TestHome />} />
       </Routes>
     </Router>
   );
